27fae008152eafd74e76031a804f85906e6b46f7
Himanshu2329 <kumarhimanshu4390@gmail.com>
2022-02-24 01:40:04 +0530

    binary done

--- Array/BinarySearch.java
+++ Array/BinarySearch.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class BinarySearch {
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+        }
+        int data=sc.nextInt();
+        int lo=0;
+        int foundAt=-1;
+        int hi=arr.length-1;
+        while (lo<=hi) {
+            int mid=(lo+hi)/2;
+            if(data<arr[mid]){
+                hi=mid-1;
+            }
+            else if(data>arr[mid]){
+                lo=mid+1;
+            }
+            else{
+                foundAt=mid;
+                break;
+            }
+        }
+        System.out.println(foundAt);
     }
 }
