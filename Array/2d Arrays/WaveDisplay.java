05671c22b3d806b54db8df43ffb82ade3a8a3a0e
Himanshu2329 <kumarhimanshu4390@gmail.com>
2022-02-27 19:03:52 +0530

    wave

---
+++ Array/2d Arrays/WaveDisplay.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class WaveDisplay {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int arr[][]=new int[m][n];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+    }
+}
