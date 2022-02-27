a7da0ee63ef698d09bbbce4b84bb5507b0299af3
Himanshu2329 <kumarhimanshu4390@gmail.com>
2022-02-28 01:33:33 +0530

    3 question done

---
+++ Array/2d Arrays/RotateBy90.java
@@ -0,0 +1,5 @@
+public class RotateBy90 {
+    public static void main(String[] args) {
+        
+    }
+}
--- Array/2d Arrays/WaveDisplay.java
+++ Array/2d Arrays/WaveDisplay.java
@@ -5,11 +5,28 @@ public class WaveDisplay {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int m=sc.nextInt();
-        int arr[][]=new int[m][n];
+        int arr[][]=new int[n][m];
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
+            for (int j = 0; j < arr[0].length; j++) {
                 arr[i][j]=sc.nextInt();
             }
+        }
+        for (int j = 0; j < arr[0].length; j++) {
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(arr[i][j]);
+            }
+
+            // for wave display
+            // if(j%2==0){
+            //     for (int i = 0; i < arr.length; i++) {
+            //         System.out.println(arr[i][j]);
+            //     }
+            // }else{
+            //     for (int i = arr.length-1; i >= 0; i--) {
+            //         System.out.println(arr[i][j]);
+            //     }
+            // }
+            // System.out.println();
         }
     }
 }
