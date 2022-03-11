f3682120dcec9516d425714601bf8330997a45a7
Himanshu2329 <kumarhimanshu4390@gmail.com>
2022-03-11 22:59:52 +0530

    String Started

---
+++ String/Intro.java
@@ -0,0 +1,17 @@
+import java.lang.invoke.StringConcatFactory;
+import java.util.Scanner;
+
+public class Intro {
+
+    public static void main(String[] args) {
+        // Scanner sc=new Scanner(System.in);
+        // String s1=sc.next();
+        // for(int i=0;i<s1.length();i++) {
+        //     char ch=s1.charAt(i);
+        //     System.out.println(ch);
+        // }       
+        String s1="himanshu ";
+            s1+="kesa h";
+            System.out.println(s1);
+    }
+}
