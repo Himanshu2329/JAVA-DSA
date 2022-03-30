424c29517d670fe991b00b69db715b5ffc7336cd
Himanshu2329 <kumarhimanshu4390@gmail.com>
2022-03-30 23:44:43 +0530

    factorial done

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Factorial {
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=Factorial(n);
+        System.out.println(a);
+    }
+    public static int Factorial(int n) {
+        if(n==0){
+            return 1;
+        }
+        int fm=Factorial(n-1);
+        int fn=n*fm;
+        return fn;
     }
 }
